741c3eb71083f987f29f58676db7251a3d021083
NaotoAsakura <82355699+NaotoAsakura@users.noreply.github.com>
2021-06-22 23:01:08 +0900

    完成

--- example.js
+++ example.js
@@ -1,34 +1,75 @@
 'use strict';
 const gameDivided = document.getElementById('game-area');
-//console.log('example.js');
+const startButton = document.getElementById('start');
+let score = 0;
+let question = 1;
+let nowcolor;
+let correct;
 
-/*if (confirm('READY?')){
-    randomcolor();
-}*/
+startButton.onclick = () =>{
+    if(window.confirm('Ready?')){
+        gameDivided.innerHTML = randomname();
+        nowcolor = randomcolor();
+        gameDivided.style.color = nowcolor;
+        correct = nowcolor.slice(0,1);
+        console.log(correct);
+        document.onkeydown = () => {
+            if(event.key === correct){
+                console.log('ok');
+                score++;
+                question++;
+            }else{
+                console.log('no');
+                question++;
+            }
+        }
+        document.onkeyup = () => {//キーを離すたび名前と色が変わる
+            gameDivided.innerHTML = randomname();
+            nowcolor = randomcolor();
+            gameDivided.style.color = nowcolor;
+            correct = nowcolor.slice(0,1);
+            console.log(correct);
+        }
 
-document.getElementById('start').click(
-    //randomcolor()
-    //console.log('クリックされました')
-);
+        // カウントダウンする秒数
+        var sec = 10;
+        // 開始日時を設定
+        var dt = new Date();
+        console.log("Start: ", dt);
+        // 終了時刻を開始日時+カウントダウンする秒数に設定
+        var endDt = new Date(dt.getTime() + sec * 1000);
+        console.log("End : ", endDt);
+        // 1秒おきにカウントダウン
+        var cnt = sec;
+        var id = setInterval(function(){
+           cnt--;
+            //console.log(cnt);
+           // 現在日時と終了日時を比較
+           dt = new Date();
+           if(dt.getTime() >= endDt.getTime()){
+             clearInterval(id);
+             document.write("Finish!" + 'あなたのスコアは' + score + '/' + question + 'です。');//スコア(0/0)を表示して終了
+            }
+        }, 1000);
+    }
+};
 
 
+const samplecolor = [
+    'red', 'blue', 'green', 'yellow', 'purple', 'orange', 'white'
+];
 
 const colorname = [
-    '赤', '青', '緑', '黄', '紫'
-]; //0,   1,    2,    3,     4  の5種類
+    '赤', '青', '緑', '黄', '紫', '橙', '白'
+];//R,    B,   G,    Y,   P,    O,    Wが正解
 
-/**
- * STARTを押すとランダムに色の名前を表示する関数
- * @param {}
- * @return{string} 色の名前
- */
+function randomcolor(){//関数 @param {} @return{string} 色の名前
+    var random1 = Math.floor(Math.random() * 7);
+    return samplecolor[random1];
+}
 
-let startTime = null;
-function randomcolor() {
-  //TODO 30秒間ランダムに色を表示する関数を実装する
-    //startTime = Date.now();
-    console.log('スタートしました');
 
-    let now = new Date();
-    let gametimeseconds = (now.getTime() - startTime);
+function randomname() {//ランダムに色の名前を表示する関数 @param {} @return{string} 色
+    var random2 = Math.floor(Math.random() * 7);
+    return colorname[random2];
 }
