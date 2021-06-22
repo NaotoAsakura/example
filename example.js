2c7b61409c3833207615ab83122f1b88a6ea058d
NaotoAsakura <82355699+NaotoAsakura@users.noreply.github.com>
2021-06-22 23:44:23 +0900

    js微修正

--- example.js
+++ example.js
@@ -8,6 +8,27 @@ let correct;
 
 startButton.onclick = () =>{
     if(window.confirm('Ready?')){
+        // カウントダウンする秒数
+        var sec = 10;
+        // 開始日時を設定
+        var dt = new Date();
+        console.log("Start: ", dt);
+        // 終了時刻を開始日時+カウントダウンする秒数に設定
+        var endDt = new Date(dt.getTime() + sec * 1000);
+        console.log("End : ", endDt);
+        // 1秒おきにカウントダウン
+        var cnt = sec;
+        var id = setInterval(function(){
+           cnt--;
+            //console.log(cnt);
+           // 現在日時と終了日時を比較
+           dt = new Date();
+           if(dt.getTime() >= endDt.getTime()){
+             clearInterval(id);
+             document.write("Finish!" + 'あなたのスコアは' + score + '/' + question + 'です。');//スコア/問題数を表示して終了
+            }
+        }, 1000);
+        
         gameDivided.innerHTML = randomname();
         nowcolor = randomcolor();
         gameDivided.style.color = nowcolor;
@@ -30,27 +51,6 @@ startButton.onclick = () =>{
             correct = nowcolor.slice(0,1);
             console.log(correct);
         }
-
-        // カウントダウンする秒数
-        var sec = 10;
-        // 開始日時を設定
-        var dt = new Date();
-        console.log("Start: ", dt);
-        // 終了時刻を開始日時+カウントダウンする秒数に設定
-        var endDt = new Date(dt.getTime() + sec * 1000);
-        console.log("End : ", endDt);
-        // 1秒おきにカウントダウン
-        var cnt = sec;
-        var id = setInterval(function(){
-           cnt--;
-            //console.log(cnt);
-           // 現在日時と終了日時を比較
-           dt = new Date();
-           if(dt.getTime() >= endDt.getTime()){
-             clearInterval(id);
-             document.write("Finish!" + 'あなたのスコアは' + score + '/' + question + 'です。');//スコア/問題数を表示して終了
-            }
-        }, 1000);
     }
 };
 
